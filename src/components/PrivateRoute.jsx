9eb7532be55178f70f2f25cb0a7aed385698bd0e
Poyraz Avsever <poyrazavsever@hotmail.com>
2022-10-01 22:00:26 +0300

    feat: add redux toolkit

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import {Navigate, useLocation} from "react-router-dom"
+import {useSelector} from "react-redux"
 
 function PrivateRoute({children}) {
-  const user = false
+
+  const user = useSelector(state => state.auth.user)
   const location = useLocation()
+  
   if(!user){
     return <Navigate to="/auth/login" replace={true} state={{
         return_url: location.pathname
---
+++ src/store/auth.js
@@ -0,0 +1,20 @@
+import {createSlice} from "@reduxjs/toolkit";
+import { act } from "react-dom/test-utils";
+
+const initialState = {
+    user:false
+}
+
+const auth = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setUser : (state, action) => {
+            state.user = action.payload
+        }
+    }
+})
+
+
+export const {setUser} = auth.actions
+export default auth.reducer
