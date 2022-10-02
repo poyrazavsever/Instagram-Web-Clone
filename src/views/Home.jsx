21dfec0c5e56a773311732d72b73a30fb800d137
Poyraz Avsever <poyrazavsever@hotmail.com>
2022-10-02 18:31:39 +0300

    feat(!): delete home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import Stories from "../components/Stories";
-import Contents from "../components/Contents";
-import Navbar from "../components/Navbar"
 
 function Home() {
     return (
-        <div className='bg-bg w-full h-screen flex flex-col items-center gap-12'>
+        <div>
 
-            <Navbar/>    
-
-            <Stories />
-
-            <Contents />
+            Homepage
             
         </div>
     )
