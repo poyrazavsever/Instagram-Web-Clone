33f8a569bb1a132c283eea1e7d9c7ddcc2c4fc90
Poyraz Avsever <poyrazavsever@hotmail.com>
2022-10-02 22:40:22 +0300

    chore: new loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {MdiInstagram, LogoInstagram} from "../icons"
 
 function Loader() {
   return (
-    <div className='w-full h-full fixed top-0 left-0 bg-black text-white flex items-center justify-center text-2xl' >
-        Loading...
+    <div className='w-full h-full fixed top-0 left-0 bg-black text-white flex flex-col gap-3 items-center justify-center text-2xl' >
+        <MdiInstagram className="text-7xl animate-spin"/>
+        <LogoInstagram className="text-3xl" />
     </div>
   )
 }
--- src/icons.js
+++ src/icons.js
@@ -131,3 +131,17 @@ export function FaFacebookOfficial(props) {
     <svg width="1em" height="1em" viewBox="0 0 1536 1536" {...props}><path fill="currentColor" d="M1451 0q35 0 60 25t25 60v1366q0 35-25 60t-60 25h-391V941h199l30-232h-229V561q0-56 23.5-84t91.5-28l122-1V241q-63-9-178-9q-136 0-217.5 80T820 538v171H620v232h200v595H85q-35 0-60-25t-25-60V85q0-35 25-60T85 0h1366z"></path></svg>
   )
 }
+
+
+export function MdiInstagram(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"></path></svg>
+  )
+}
+
+
+export function MdiCloseCircle(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M12 2c5.53 0 10 4.47 10 10s-4.47 10-10 10S2 17.53 2 12S6.47 2 12 2m3.59 5L12 10.59L8.41 7L7 8.41L10.59 12L7 15.59L8.41 17L12 13.41L15.59 17L17 15.59L13.41 12L17 8.41L15.59 7Z"></path></svg>
+  )
+}
